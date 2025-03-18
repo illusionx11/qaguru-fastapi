e9163c1df13f1053b904835b65f52c627afcbf97
illusionx11 <84992229+illusionx11@users.noreply.github.com>
2025-03-18 17:23:21 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,16 +5,23 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
-from routes import status, users
-from database.engine import create_db_and_tables
+from app.routes import status, users
+from app.database.engine import create_db_and_tables
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("On startup")
+    create_db_and_tables()
+    yield
+
+    print("On shutdown")
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 add_pagination(app)
 
 if __name__ == "__main__":
-    create_db_and_tables()
     APP_URL = os.getenv("APP_URL").split("http://")[1]
     APP_HOST = APP_URL.split(":")[0]
     APP_PORT = int(APP_URL.split(":")[1])
